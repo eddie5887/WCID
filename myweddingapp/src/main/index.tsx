7e5dc672d93b3d9d93584c48481bcb14ce8cae20
eddie_p <eddie_p@greypanel.com>
2023-07-14 11:07:05 +0800

    save

--- myweddingapp/src/main/index.tsx
+++ myweddingapp/src/main/index.tsx
@@ -6,22 +6,7 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 const Main: FC = () => {
   return (
     <div >
-      <Parallax pages={4}>
-        <ParallaxLayer offset={0} >
-          <Image src={invitationCard} fit="fill" />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={1}>
-          <Image src={invitationCard} fit="fill" style={{paddingLeft:200,paddingRight:200}}/>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={1.5}>
-          <Image src={bainianhaohe} fit="fill" />
-        </ParallaxLayer>
-      </Parallax>
-      <Parallax pages={4}>
-      <ParallaxLayer offset={0} >
-          <Image src={invitationCard} fit="fill" />
-        </ParallaxLayer>
-      </Parallax>
+ 
     </div>
   );
 };
